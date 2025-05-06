7cd2e7790cbdbe30b1b30dbb32ad5bd5ca6b3566
Mornac <imornac@gmail.com>
2025-05-06 17:45:15 +0200

    suppr the f variable

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -4,9 +4,9 @@ import sys
 def factorial(n):
     result = 1
     while n > 1:
-        result *= n  # <-- il ne faut pas mettre n -= 1
+        result *= n
+        # Erreur ici : il manque 'n -= 1', ce qui rend la boucle infinie
     return result
 
-f = factorial(int(sys.argv[1]))
-print(f)
+print(factorial(int(sys.argv[1])))
 
