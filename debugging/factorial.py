a0a06a16a48f2772ec4bde98423aedad043fe318
Mornac <imornac@gmail.com>
2025-05-06 17:08:43 +0200

    version avec bug

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -4,8 +4,7 @@ import sys
 def factorial(n):
     result = 1
     while n > 1:
-        result *= n
-        n -= 1
+        result *= n  # <-- il ne faut pas mettre n -= 1
     return result
 
 f = factorial(int(sys.argv[1]))
