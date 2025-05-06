a248b5b50ae5f509e9932f0ae79dd9115be14c2c
Mornac <imornac@gmail.com>
2025-05-06 12:26:37 +0200

    delete lines after line 12

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -8,17 +8,5 @@ def factorial(n):
         n -= 1  # Décrémentation nécessaire
     return result
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python3 script.py <number>")
-    else:
-        try:
-            num = int(sys.argv[1])
-            if num < 0:
-                print("Le factoriel n'est pas défini pour les entiers négatifs.")
-            else:
-                f = factorial(num)
-                print(f)
-        except ValueError:
-            print("Veuillez entrer un entier valide.")
-
+f = factorial(int(sys.argv[1]))
+print(f)
